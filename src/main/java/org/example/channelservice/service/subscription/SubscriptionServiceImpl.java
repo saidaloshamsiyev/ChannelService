17ab1345d4170aa55777dc489d478a625a37d257
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-10-12 11:29:38 +0500

    channel update

--- src/main/java/org/example/channelservice/service/subscription/SubscriptionServiceImpl.java
+++ src/main/java/org/example/channelservice/service/subscription/SubscriptionServiceImpl.java
@@ -84,8 +84,8 @@ public class SubscriptionServiceImpl implements SubscriptionService {
             throw new BaseException("Subscription not found", HttpStatus.NOT_FOUND.value());
         }
         return SubscriptionResponse.builder()
-                .channelId(subscriptionEntityList.getFirst().getChannelId())
-                .subscriberId(subscriptionEntityList.getFirst().getChannelId())
+                .channelId(subscriptionEntityList.get(1).getChannelId())
+                .subscriberId(subscriptionEntityList.get(1).getChannelId())
                 .build();
     }
 
