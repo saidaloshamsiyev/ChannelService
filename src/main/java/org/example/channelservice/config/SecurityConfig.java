c5f35fbf37bb9380a83a75789c980ab1df2f9ac7
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-10-13 20:57:35 +0500

    channel

--- src/main/java/org/example/channelservice/config/SecurityConfig.java
+++ src/main/java/org/example/channelservice/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 public class SecurityConfig {
-    private final String[] WHITE_LIST = {"/api/auth/login", "/api/auth/register", "/api/auth/swagger-ui/", "/api/auth/v3/api-docs/"};
+    private final String[] WHITE_LIST = {"/api/auth/login", "/api/auth/register", "/api/auth/swagger-ui/**", "/api/auth/v3/api-docs/**"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
