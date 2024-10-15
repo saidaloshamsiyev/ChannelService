43470ce8f6c88b34e090e602a2de269ec5279fce
BehzodFarkhodov <behzodfarhodov13@gmail.com>
2024-10-15 16:09:19 +0500

    add swagger configuration for channelService

--- src/main/java/org/example/channelservice/config/SecurityConfig.java
+++ src/main/java/org/example/channelservice/config/SecurityConfig.java
@@ -4,13 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-    private final String[] WHITE_LIST = {"/api/auth/login", "/api/auth/register", "/api/auth/swagger-ui/**", "/api/auth/v3/api-docs/**"};
+    private final String[] WHITE_LIST = {"/api/auth/login", "/api/auth/register",
+            "/api/auth/swagger-ui/**",
+            "/api/auth/v3/api-docs/**",
+            "/api/channel/swagger-ui/**",
+            "/api/channel/v3/api-docs/**",
+            "/v3/api-docs/",
+            "/swagger-ui/",
+            "/swagger-ui.html"
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
