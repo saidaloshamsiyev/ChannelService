828389db7837958dcab2dda9165e8fd169b96be1
BehzodFarkhodov <behzodfarhodov13@gmail.com>
2024-10-18 15:44:12 +0500

    test commit for pull

--- src/main/java/org/example/channelservice/filter/CustomFilter.java
+++ src/main/java/org/example/channelservice/filter/CustomFilter.java
@@ -13,44 +13,6 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class CustomFilter extends OncePerRequestFilter {
-    //    private final String SECRET_KEY = "qwefklsdfj3roif134adsj14fdslkjf1234fiodwq";
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        String authorizationHeader = request.getHeader("Authorization");
-//        // Token mavjudligini tekshirish
-//        if (Objects.isNull(authorizationHeader) || !authorizationHeader.startsWith("Bearer ")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//
-//        String token = authorizationHeader.substring(7);
-//        try {
-//            // JWT dan username olish
-//            Claims claims = Jwts.parser()
-//                    .setSigningKey(SECRET_KEY)
-//                    .parseClaimsJws(token)
-//                    .getBody();
-//
-//            String username = claims.getSubject();
-//
-//            // Foydalanuvchini SecurityContext ga qo'shish
-//            UsernamePasswordAuthenticationToken auth =
-//                    new UsernamePasswordAuthenticationToken(username, null, null);
-//            SecurityContextHolder.getContext().setAuthentication(auth);
-//        } catch (Exception e) {
-//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-//            return;
-//        }
-//
-//        filterChain.doFilter(request, response);
-//    }
-//}
-
-
-
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String username = request.getHeader("Authorization");
@@ -66,6 +28,5 @@ public class CustomFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 }
-
 
 
