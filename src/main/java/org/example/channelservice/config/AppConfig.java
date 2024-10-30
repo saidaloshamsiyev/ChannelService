88214442527c58572affb8d7abf869cceb6ee7d5
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-10-30 13:40:28 +0500

    channel newed

--- src/main/java/org/example/channelservice/config/AppConfig.java
+++ src/main/java/org/example/channelservice/config/AppConfig.java
@@ -21,7 +21,7 @@ public class AppConfig {
     @Bean
     public ProducerFactory<String, UserNotificationRequest> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
