3dfd8dab9a157fcaef2ba9e4517fe3bacdf68603
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-12 14:44:43 +0500

    channel newed

--- src/main/java/org/example/channelservice/clients/VideoServiceClient.java
+++ src/main/java/org/example/channelservice/clients/VideoServiceClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "VIDEO-SERVICE", url = "http://localhost:8086", configuration = FeignConfig.class)
+@FeignClient(name = "VIDEO-SERVICE", configuration = FeignConfig.class)
 public interface VideoServiceClient {
     @GetMapping("/api/video/by-channel/{channelId}")
     List<VideoResponse> getVideosByChannelId(@PathVariable("channelId") UUID channelId);
--- src/main/java/org/example/channelservice/config/FeignConfig.java
+++ src/main/java/org/example/channelservice/config/FeignConfig.java
@@ -18,7 +18,7 @@ public class FeignConfig {
 /*
             String roles = authentication.getAuthorities().stream().findFirst().get().getAuthority();
 */
-            requestTemplate.header("Authorization",username);
+            requestTemplate.header("X-Username",username);
             /*requestTemplate.header("X-Roles",roles);*/
 
         };
